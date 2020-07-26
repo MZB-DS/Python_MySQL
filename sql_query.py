ca22e3861816a7a1648426f6148ac387ad4c2a63
MZB-DS <49339352+MZB-DS@users.noreply.github.com>
2020-07-26 20:58:40 +0530

    Create sql_query.py

---
+++ sql_query.py
@@ -0,0 +1,6 @@
+from sql_query_to_df import get_data
+
+my_sql_query = '''select name, address, contact, email, city from people_information where city="PUNE";'''
+column_names = ["Name", "Address", "Contact", "Email", "City"]
+sql_query_output_df = get_data(my_sql_query, column_names)
+print(sql_query_output_df.head())
